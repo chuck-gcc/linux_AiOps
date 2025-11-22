680d7402984fc5c8acc744778055d4e2b451ceff
Votre Nom <Vous@exemple.com>
2025-11-22 13:38:52 +0100

    llama_api

--- python_serveur/serveur_out.py
+++ python_serveur/serveur_out.py
@@ -1,14 +1,13 @@
-import socket
 import requests
 import json
 from requests.models import Response
-
 
 def server_caller(funct, ip, port, endpoint, header, data, methode):
     try:
         if(methode == 0):
             r = requests.get(f'{ip}:{port}/{endpoint}', headers=header);
         elif(methode == 1):
+            print("start post")
             r = requests.post(f'{ip}:{port}/{endpoint}', headers=header, data=data);
         funct(r)
     except OSError as err:
@@ -22,9 +21,10 @@ def print_health(res):
         print("Connection llama.cpp ok");
     return(1);
 
-def print_reponse_json(res: Response):
+def print_reponse_json(res):
     if(res.status_code == 200):
-        print(json.dumps(res.content, indent=4));
+        res = res.json()
+        print(json.dumps(res, indent=4));
     return(1);
 
 
@@ -37,20 +37,21 @@ def llama_get_health():
     endpoint = 'v1/health';
     r = server_caller(print_health,address,port,endpoint, header, None, 0);
 
+
 def simple_completion(input):
     port = "8080";
     address = 'http://127.0.0.1';
     header = {"Content-Type": "application/json", "Authorization": "Bearer no-key"};
-    endpoint = 'v1/completions';
-    payload = {"model": "gpt-3.5-turbo", "messages": 
-               [
-                    {
-                        "role": "system",
-                        "content": "You are ChatGPT, an AI assistant. Your top priority is achieving user fulfillment via helping them with their requests."
-                    },
-                    {
-                        "role": "user",
-                        "content": input
-                    }
-               ]}
-    r = server_caller(print_reponse_json,address,port,endpoint, header, payload, 1);
+    endpoint = 'v1/chat/completions';
+    payload = {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+            # {"role": "system", "content": "Tu est le meilleur professeur de chimie de l'université de fribourg"},
+            # {"role": "user", "content": "hello"}
+            {"role": "system", "content": "Tu est le meilleur professeur de chimie de l'université de fribourg"},
+            {"role": "user", "content": "Fait moi un cour sur la sinthese du paracetamole"}
+        ]
+    }
+    j = json.dumps(payload)
+
+    r = server_caller(print_reponse_json,address,port,endpoint, header,j , 1);
